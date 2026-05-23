c632a517a42253288632dccd3a2d34715aa8816c
alejandracuellar <alejandra.cuellarlopez@uptc.edu.co>
2026-05-23 18:46:40 -0500

    Contrato actualizado

---
+++ src/main/java/edu/uptc/Contrato.java
@@ -0,0 +1,83 @@
+package edu.uptc;
+
+import java.time.LocalDate;
+
+public class Contrato {
+    private String numeroContrato;
+    private LocalDate fechaCreacion;
+    private Contratante contratante;
+    private Contratista contratista;
+    private double valorContrato;
+    private LocalDate plazoEjecucion;
+    private String estado;
+
+    public Contrato(String numeroContrato, LocalDate fechaCreacion, Contratante contratante, Contratista contratista, double valorContrato, LocalDate plazoEjecucion, String estado) {
+        this.numeroContrato = numeroContrato;
+        this.fechaCreacion = fechaCreacion;
+        this.contratante = contratante;
+        this.contratista = contratista;
+        this.valorContrato = valorContrato;
+        this.plazoEjecucion = plazoEjecucion;
+        this.estado = estado;
+    }
+
+    public Contrato(){
+
+    }
+
+    public String getNumeroContrato() {
+        return numeroContrato;
+    }
+
+    public void setNumeroContrato(String numeroContrato) {
+        this.numeroContrato = numeroContrato;
+    }
+
+    public LocalDate getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDate fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Contratante getContratante() {
+        return contratante;
+    }
+
+    public void setContratante(Contratante contratante) {
+        this.contratante = contratante;
+    }
+
+    public Contratista getContratista() {
+        return contratista;
+    }
+
+    public void setContratista(Contratista contratista) {
+        this.contratista = contratista;
+    }
+
+    public double getValorContrato() {
+        return valorContrato;
+    }
+
+    public void setValorContrato(double valorContrato) {
+        this.valorContrato = valorContrato;
+    }
+
+    public LocalDate getPlazoEjecucion() {
+        return plazoEjecucion;
+    }
+
+    public void setPlazoEjecucion(LocalDate plazoEjecucion) {
+        this.plazoEjecucion = plazoEjecucion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
